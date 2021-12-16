df659bbc60431dced77738e7e387adf137852ab3
Affaxed <delhinrharl@gmail.com>
2021-12-16 12:04:47 +0000

    Add functions and jest tests

--- capitals/capitalize.test.js
+++
@@ -1,9 +0,0 @@
-const capitalize = require("./capitals.js");
-
-const str = "affax";
-const capitalStr = "Affax";
-const word = capitalize(str);
-
-test("should return string with capitalized first char", () => {
-  expect(word).toBe(capitalStr);
-});
